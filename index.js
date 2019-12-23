2df21f012fcb5f1b88b4651960288713fd466611
plbin97 <i@teenet.me>
2019-12-22 22:15:29 -0500

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(cors());
 async function service() {
-    app.post("/newDatasetRecord", )
+    app.post("/newDatasetRecord", await require('./src/controller/newDataset'));
     app.get("/verifyDatasetTokenAndDatasetName", await require('./src/controller/verifyDatasetTokenAndDatasetName'));
     app.listen(config.listenOnPort);
 }
--- src/controller/newDataset.js
+++ src/controller/newDataset.js
@@ -1,6 +1,46 @@
+let encryption = require("../microservice-communication-encryption/index");
+let DatasetClass = require('../model/datasetInfoClass');
 
-function newDataset(request, response) {
+async function newDataset(request, response) {
+    if(
+        request.body["datasetName"] === undefined ||
+        request.body["datasetPublicity"] === undefined ||
+        request.body["datasetMetaData"] === undefined ||
+        request.body["datasetOwnerList"] === undefined ||
+        request.body["datasetAdminList"] === undefined
+    ) {
+        response.send("Lack of parameter");
+        return null;
+    }
+    let decripted = {
+        name: encryption.decrypt(request.body["datasetName"]),
+        publicity: encryption.decrypt(request.body["datasetPublicity"]),
+        metaData: encryption.decrypt(request.body["datasetMetaData"]),
+        ownerList: encryption.decrypt(request.body["datasetOwnerList"]),
+        adminList: encryption.decrypt(request.body["datasetAdminList"])
+    };
 
+    for (let i in decripted) {
+        if (i == null) {
+            response.send("decryptionFailed");
+            return null;
+        }
+    }
+    let dataset = new DatasetClass();
+    await dataset.connect().catch();
+    let newDatasetResult = await dataset.putNewDatasetInfo(
+        decripted.name,
+        Number(decripted.publicity),
+        decripted.metaData,
+        JSON.parse(decripted.ownerList),
+        JSON.parse(decripted.adminList)
+    );
+
+    if (newDatasetResult.result.ok !== 1) {
+        response.send("Server error");
+        return null;
+    }
+    response.send("0");
 }
 
 module.exports = newDataset;
